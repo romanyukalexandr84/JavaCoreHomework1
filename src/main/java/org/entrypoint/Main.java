de5c37aa0602be37420502f50d090b5bdad1a2d8
romanyukalexandr84 <romanyukalexandr84@gmail.com>
2023-12-20 05:02:39 +0300

    add entrypoint and database

---
+++ src/main/java/org/entrypoint/Main.java
@@ -0,0 +1,12 @@
+package org.entrypoint;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Введите заметку");
+        Scanner scan = new Scanner(System.in);
+        String note = scan.next();
+    }
+}
